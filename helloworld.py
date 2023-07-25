9e0ea70823120ef6eab421cbe2ba00c5cb51f5bc
hksamantha <hksamantha5@gmail.com>
2023-07-25 23:55:31 +0800

    third

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,9 @@
 print("hello world") # comment
 age = 100 # Integer
 name = "John" # String
+is_female = False
 print(type(age))
 print(type(name))
 is_student= True
 print(type(is_student))
+print(type(is_female))
